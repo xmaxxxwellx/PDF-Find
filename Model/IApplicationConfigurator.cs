81215d42c568ea12d8b1e43e45f580e80be67203
Maxim <Maxim@MAXIM->
2017-10-07 15:03:41 +0300

    registry changes

--- Model/IApplicationConfigurator.cs
+++ Model/IApplicationConfigurator.cs
@@ -2,16 +2,12 @@ using System.ComponentModel;
 
 namespace Model
 {
-    public enum Language
-    {
-        English,
-        Ukrainian
-    }
-
     public interface IApplicationConfigurator : INotifyPropertyChanged
     {
-        Language Language { get; set; }
+        string Language { get; set; }
         string ReaderPath { get; set; }
         string DataBaseConnectionString { get; set; }
+
+        void Save();
     }
 }
--- Model/RegistryApplicationConfigurator.cs
+++ Model/RegistryApplicationConfigurator.cs
@@ -1,3 +1,5 @@
+
+using Microsoft.Win32;
 using System;
 using System.ComponentModel;
 using System.IO;
@@ -9,9 +11,11 @@ namespace Model
     {
         #region Fields
 
-        private Language _language;
+        private string _language;
         private string _readerPath;
         private string _dataBaseConnectionString;
+
+        private string _regKey;
 
         #endregion
 
@@ -19,15 +23,16 @@ namespace Model
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public Language Language
+        public string Language
         {
             get { return _language; }
             set
             {
-                if (!Enum.IsDefined(typeof (Language), value)) throw new ArgumentOutOfRangeException(nameof(value));
+                if (string.IsNullOrWhiteSpace(value))
+                    // todo locale
+                    throw new ArgumentException("Argument is null or whitespace", nameof(value));
                 if (_language == value) return;
                 _language = value;
-                // todo change in Registry
                 OnPropertyChanged();
             }
         }
@@ -51,7 +56,6 @@ namespace Model
                 // use fullPath or name if program writen in Path?
 
                 _readerPath = Path.GetFullPath(value);
-                // todo set to Registry
                 OnPropertyChanged();
             }
         }
@@ -65,22 +69,48 @@ namespace Model
                     // todo locale
                     throw new ArgumentException("Argument is null or whitespace", nameof(value));
                 _dataBaseConnectionString = value;
-                // todo set to Registry
                 OnPropertyChanged();
             }
         }
 
         #endregion
 
-        public RegistryApplicationConfigurator()
+        public RegistryApplicationConfigurator(string regKey)
         {
-            throw new NotImplementedException("Open Registry to take/write data");
+            _regKey = regKey;
+
+            RegistryKey currentUser = Registry.CurrentUser;
+            RegistryKey pdfFindKey = currentUser.OpenSubKey(regKey);
+
+            if (pdfFindKey != null) {
+
+                Language = pdfFindKey.GetValue("language").ToString();
+                ReaderPath = pdfFindKey.GetValue("readerPath").ToString();
+                DataBaseConnectionString = pdfFindKey.GetValue("dbConnectionString").ToString();
+
+                pdfFindKey.Close();
+            }
+            else {
+                _language = "english";
+                _readerPath = "";
+                _dataBaseConnectionString = "";
+            }
         }
 
         #region Methods
 
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public void Save() {
+            RegistryKey currentUser = Registry.CurrentUser;
+            RegistryKey pdfFindKey = currentUser.OpenSubKey(_regKey, true) ?? currentUser.CreateSubKey(_regKey, true);
+
+            pdfFindKey.SetValue("language", Language);
+            pdfFindKey.SetValue("readerPath", ReaderPath);
+            pdfFindKey.SetValue("dbConnectionString", DataBaseConnectionString);
+            pdfFindKey.Close();
+        }
 
         #endregion
     }

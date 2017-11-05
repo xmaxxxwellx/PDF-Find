ece1036181176f2f4c2bee23961d65c05b37662b
sexol123 <sergeimaleev@gmail.com>
2017-11-05 13:40:02 +0200

    aded StateReport class

---
+++ ViewModel/StateReport.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Model.Entities;
+
+namespace ViewModel
+{
+   public class StateReport
+   {
+       public static StateReport Instance { get; } = new StateReport();
+
+        private ReportConfiguration report;
+
+        public ReportConfiguration CurrentReportConfiguration
+        {
+            get { return report; }
+            set
+            {
+                if (value == null) return;
+                report = value;
+            }
+       }
+
+       public ICommand SaveCommand { get; }
+
+       private StateReport()
+       {
+          
+       }
+    }
+}
